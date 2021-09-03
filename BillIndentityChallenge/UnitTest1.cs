412ecf7c720483e6368b89d3cdc2958350618dc3
Mukesh Rauniyar <mukesh@prabhumanagement.com>
2021-09-03 20:33:30 +0545

    Add project files.

---
+++ BillIndentityChallenge/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BillIndentityChallenge
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
